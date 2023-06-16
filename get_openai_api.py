b3fd831bcdabdb565d39861b25d8c0917973554a
Lumiin0us <73905501+Lumiin0us@users.noreply.github.com>
2023-06-17 03:08:19 +0500

    accessed openai's API & basic UI setup for the app

---
+++ get_openai_api.py
@@ -0,0 +1,15 @@
+import requests 
+import openai 
+
+API_KEY = "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"
+openai.api_key = API_KEY
+
+completion_response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages=[{
+        'role': 'user', 
+        'content': 'Write an essay about penguins',
+    }]
+)
+
+print(completion_response.choices[0].message.content)
